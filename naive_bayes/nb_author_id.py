14ca4f85544c9cfbe09b37ce87ebc7b20e4c7d63
Pavel Chernovsky <pchernov@cisco.com>
2015-10-29 14:04:44 +0200

    my project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,10 +23,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+t1 = time()
+train_time = t1 - t0
+t0 = time()
+pred = clf.predict(features_test)
+t1 = time()
+predict_time = t1 - t0
+
+accuracy = accuracy_score(labels_test, pred)
+longer = 'predict' if (train_time-predict_time)<0 else'train'
+print 'accuracy: {} train time: {} predict time: {}; it takes more time to: {}'.\
+    format(accuracy, train_time, predict_time, longer)
 
 #########################################################
 
