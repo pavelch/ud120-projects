778b0aff3efccb82a97c466d1a8d0f6a7abc204e
Pavel Chernovsky <pchernov@cisco.com>
2015-11-02 14:04:50 +0200

    lesson 4 completed

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,25 +17,31 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import accuracy_score
+
+clf = AdaBoostClassifier(n_estimators=25)
+scores = cross_val_score(clf, features_train, labels_train)
+print 'scores mean: {}'.format(scores.mean())
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+accuracy = accuracy_score(labels_test, pred)
+print 'accuracy: {}'.format(accuracy)
 ### visualization code (prettyPicture) to show you the decision boundary
-
-
-
-
-
-
 
 
 try:
