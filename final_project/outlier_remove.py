822e7166b7520b36c820b7ae3ccaa6e18c8fb450
Pavel Chernovsky <pchernov@cisco.com>
2015-12-06 21:51:47 +0200

    removing outliers in progress

--- final_project/outlier_remove.py
+++ final_project/outlier_remove.py
@@ -2,7 +2,6 @@
 import pickle
 import numpy as np
 from sympy.physics.quantum.circuitplot import matplotlib
-
 from tools.feature_format import featureFormat, targetFeatureSplit
 
 
@@ -11,16 +10,32 @@ from tools.feature_format import featureFormat, targetFeatureSplit
 #     target = data[:, 0]
 #     features = data[:, 1:]
 #     return target, features
-def reject_outliers(data, m = 2):
+def collect_svalues(data):
     svals = []
     for i in range(1, data.shape[1]):
         column = data[:, i]
         d = np.abs(column - np.median(column))
         mdev = np.median(d)
-        s = d/mdev if mdev else 0.
+        s = d / mdev if mdev else 0.
         svals.append(s)
 
+    return svals
 
+
+def drop_outliers(data):
+    svals = collect_svalues(data)
+    leavein = []
+    out = []
+    for inx, item in enumerate(svals[0]):
+        compare_with = [row[inx] for row in svals]
+        compare = map(lambda x: x < 10, compare_with)
+        print '{}, {}'.format(compare_with, compare)
+        if False not in compare:
+            leavein.append(data[inx])
+        else:
+            out.append(data[inx])
+
+    return np.array(leavein), np.array(out)
 
 
 features_list = ['poi', 'salary', 'bonus']
@@ -28,12 +43,13 @@ with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 data = featureFormat(data_dict, features_list)
 
-data_cleaned = reject_outliers(data)
-for point in data:
+svalues = collect_svalues(data)
+
+data_cleaned, outliers = drop_outliers(data)
+for point in outliers:
     salary = point[1]
     bonus = point[2]
     matplotlib.pyplot.scatter(salary, bonus)
-
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
